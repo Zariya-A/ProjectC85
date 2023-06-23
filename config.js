a9ea5251c5e8c6de0b30ee2b867c30bca83be2f4
Zariya-A <84050806+Zariya-A@users.noreply.github.com>
2023-06-22 21:00:12 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyByMQ8sWe90IDZFxnN1El68ftiH6Sfi_bU",
+  authDomain: "spectagram-88129.firebaseapp.com",
+  databaseURL: "https://spectagram-88129-default-rtdb.firebaseio.com",
+  projectId: "spectagram-88129",
+  storageBucket: "spectagram-88129.appspot.com",
+  messagingSenderId: "883408276898",
+  appId: "1:883408276898:web:40271c54ff7ff231a2f6fb"
+  };
